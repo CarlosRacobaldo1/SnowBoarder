c9c865c881065773c741652fddb51c56758f56d6
Carlos Henrique Racobaldo Luz Montes <79218808+CarlosRacobaldo1@users.noreply.github.com>
2025-02-16 18:38:42 -0300

    Feat: Quality of live
    When player dies he is unable to control the character until respawn, furthermore death sfx and effects wil be only played once, instead of every time the player hits its head

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,15 +8,19 @@ public class CrashDetector : MonoBehaviour
      [SerializeField] float delay=1f;
      [SerializeField] ParticleSystem crashEffect;
      [SerializeField] AudioClip crashSFX;
+     bool hasCrashed = false;
    void OnTriggerEnter2D(Collider2D other) {
         
-        if(other.tag == "Ground") {
+        if(other.tag == "Ground" && !hasCrashed) {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControl();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene",delay);
         }
     }
      void ReloadScene(){
+            hasCrashed = false;
             SceneManager.LoadScene(0);
         }
 }
